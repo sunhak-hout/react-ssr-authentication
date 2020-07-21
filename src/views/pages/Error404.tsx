3246c570911326467c6da0596f481032965c8a30
sunhakhout <sunhak.hout@gmail.com>
2020-07-21 22:07:18 +0700

    style: Error404 page

--- src/views/pages/Error404.tsx
+++ src/views/pages/Error404.tsx
@@ -1,12 +1,35 @@
-import { Typography } from "@material-ui/core";
-import React, { Fragment } from "react";
+import { Box, Button, createStyles, makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import error404SVG from '../../../public/undraw_page_not_found_su7k.svg';
+import LayoutCenter from '../layouts/LayoutCenter';
 
 const Error404: React.FC = () => {
+  const c = useStyles();
   return (
-    <Fragment>
-      <Typography>Page Not Found - 404</Typography>
-    </Fragment>
+    <LayoutCenter>
+      <Box textAlign="center">
+        <img src={`/${error404SVG}`} className={c.svg} />
+        <Typography variant="h5" gutterBottom>
+          Page not found!
+        </Typography>
+        <Typography gutterBottom>Please make sure the URL is correct.</Typography>
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <Button color="primary">Back to home page</Button>
+        </Link>
+      </Box>
+    </LayoutCenter>
   );
 };
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    svg: {
+      width: 520,
+      maxWidth: '70%',
+      marginBottom: 24,
+    },
+  }),
+);
 
 export default Error404;
