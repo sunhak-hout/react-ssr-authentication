6b310320c1874eb83d4bb3e329bf847fee134234
sunhakhout <sunhak.hout@gmail.com>
2020-07-22 01:17:06 +0700

    fix: mui duplicate class name

--- src/views/contexts/index.tsx
+++ src/views/contexts/index.tsx
@@ -1,20 +1,32 @@
+import {
+  createGenerateClassName,
+  CssBaseline,
+  StylesProvider,
+  ThemeProvider,
+} from '@material-ui/core';
 import React from 'react';
 import { CookiesProvider } from 'react-cookie';
+import { theme } from '../libs/theme';
 import { AuthProvider } from './useAuth';
 import { RequestProvider } from './useRequest';
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import { theme } from '../libs/theme';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'c',
+  disableGlobal: true,
+});
 
 const ContextProvider: React.FC = ({ children }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <CookiesProvider>
-        <RequestProvider>
-          <AuthProvider>{children}</AuthProvider>
-        </RequestProvider>
-      </CookiesProvider>
-    </ThemeProvider>
+    <StylesProvider generateClassName={generateClassName}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <CookiesProvider>
+          <RequestProvider>
+            <AuthProvider>{children}</AuthProvider>
+          </RequestProvider>
+        </CookiesProvider>
+      </ThemeProvider>
+    </StylesProvider>
   );
 };
 
